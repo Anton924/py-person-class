1da49c08c0bc4cd219e6fc77f7c4b87c4a0f42bd
Anton Yakovenko <yakovenkoanton2007@gmail.com>
2025-03-19 21:56:19 +0100

    Update

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ def create_person_list(people: list) -> list:
                 for partner in people:
                     partner_real_name, partner_age, *_ = partner.values()
                     if partner_name == partner_real_name:
-                        if list(person.keys())[2] == "wife":
+                        if 'wife' in person:
                             new_instance = Person(partner_real_name,
                                                   partner_age)
                             instance.wife = new_instance
